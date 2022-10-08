20192477508b4cd4576173fdd5f846530771c267
scare-destiny <zhenya.venger@gmail.com>
2022-10-08 15:36:57 +0300

    full size hero img on mobile devices

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Hero = ({ image, width, height }) => {
 	return (
-		<div className='mx-auto max-w-md sm:max-w-xl w-full md:max-w-3xl p-6  sm:py-6 md:px-20 md:py-10'>
+		<div className='mx-auto max-w-md sm:max-w-xl w-full md:max-w-3xl py-6  sm:py-6 md:px-20 md:py-10'>
 			<Image
 				src={image}
 				alt='logo'
