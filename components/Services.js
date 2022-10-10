14627045029b8eb98f1e3b6db656eaf72d5d8efb
scare-destiny <zhenya.venger@gmail.com>
2022-10-10 15:50:28 +0300

    updated  padding bottom for services container

--- components/Services.js
+++ components/Services.js
@@ -5,7 +5,7 @@ const Services = () => {
 	return (
 		<section className='text-gray-600 body-font'>
 			<div className='w-full container py-5 mx-auto'>
-				<div className='text-center mb-8'>
+				<div className='text-center pb-2'>
 					<Heading
 						text='20 - 500 MW Solar Turnkey Lines with DESERT Technology
 '
